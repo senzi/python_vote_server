27bc888c39fabd2af53eb033247ca5327ecdbb57
senzi <sennes111762@gmail.com>
2014-06-07 05:22:06 -0700

    update the english vertion

--- en_vote_server.py
+++ en_vote_server.py
@@ -12,30 +12,36 @@ def get_ip_address(ifname):
 	pktString = fcntl.ioctl(skt.fileno(), 0x8915, struct.pack('256s', ifname[:15]))
 	ipString  = socket.inet_ntoa(pktString[20:24])
 	return ipString
-
+shark = '''            __                              
+     o                 /' )                             
+                     /'   (                          ,  
+                 __/'     )                        .' `;
+  o      _.-~~~~'          ``---..__             .'   ; 
+    _.--'  b)                       ``--...____.'   .'  
+   (     _.      )).      `-._                     <    
+    `\|\|\|\|)-.....___.-     `-.         __...--'-.'.  
+      `---......____...---`.___.'----... .'         `.; 
+                                       `-`           `  
+'''
 welcome ='''
-
 __        _______ _     ____ ___  __  __ _____ 
 \ \      / / ____| |   / ___/ _ \|  \/  | ____|
  \ \ /\ / /|  _| | |  | |  | | | | |\/| |  _|  
   \ V  V / | |___| |__| |__| |_| | |  | | |___ 
    \_/\_/  |_____|_____\____\___/|_|  |_|_____|
-The voting system is written in python
-you can use this voting system to elect 
-the best food in the Southern District canteen
-
-written by panjiansen & ouyangxiaohui
-
+This voting system is written in python
+It can be voted to select the most popular food 
+in the Southern District Canteen
+                     [written by]:
+           panjiansen&ouyangxiaohui
 ******************************************************
 This work is licensed under the Creative Commons 
 Attribution 4.0 International License. 
 To view a copy of this license
 visit http://creativecommons.org/licenses/by/4.0/.
 ******************************************************
-
-if you want to quit , just ENTER "quit"
-
-ok,please Enter your Student Number:
+if you want to exit , please input "quit"
+ok,please input your student number:
 ''' 
 logo = '''
 __        _______ _     ____ ___  __  __ _____ 
@@ -48,8 +54,8 @@ __        _______ _     ____ ___  __  __ _____
 ip = get_ip_address('eth0')
 PORT = 4567
 print logo
-print 'This is the vote server.'
-print 'use:<telnet %s %s>to connect the server' % (ip,PORT)
+print 'This is the server of the voting system.'
+print 'input:<telnet %s %s>connect to system' % (ip,PORT)
 
 
 goodbye = '''
@@ -57,10 +63,12 @@ goodbye = '''
 ********************************
 *   thank you for your voting     
 *   here is the result                    
-*   ## Enter:'quit' to quit ##                
-*   Enter to update the result     
+*   Input:'quit' to quit               
+*   Hit 'Enter' to update the result     
 *   have fun  !                              
 ********************************
+@In case of cheating, a user only has one chance to log in and to vote,
+@the voting result will not be visible when you log out.
 '''
 
 class vote(LineReceiver):
@@ -69,6 +77,7 @@ class vote(LineReceiver):
 		self.users = users
 		self.realname = None
 		self.state = "get_name"
+		self.food = None
 
 	def connectionMade(self):
 		self.sendLine(welcome) 
@@ -86,19 +95,26 @@ class vote(LineReceiver):
 		if number == "quit" :
 			self.transport.loseConnection()
 		elif self.users.has_key(number):
-			self.sendLine("this number is used !")
+			self.sendLine("This number is existed, please input again!")
 			return
 		elif len(number) == 10:
+			try:
+				temp = int(number)
+			except ValueError:
+				self.sendLine("please do not input nonnumeric character")
+				return
 			self.realname = check_name(number)
 			if self.realname == 'unknow_name':
-				self.sendLine("hello,but i think you are not in this class.(?)")
+				self.sendLine("Hello, but I think you are not in this class.(?)")
 				return
-			self.sendLine("Hello %s ,please pick a number from 1 to 10  " % (self.realname))
+			self.sendLine(shark)
+			print_food(self)
+			self.sendLine("\nHello %s, please pick a number from 1 to 18." % (self.realname))
 			self.number = number
 			self.users[number] = self
 			self.state = "vote"
 		else :
-			self.sendLine("The number should be 10 number ,please try again!")
+			self.sendLine("The student number should be 10 digits, please try again!")
 			return 
 	def handle_vote(self, message):
 		if message == "quit" :
@@ -107,19 +123,20 @@ class vote(LineReceiver):
 		try:
 			message = int(message)
 		except ValueError:
-			self.sendLine("please Enter the right number you want to vote")
+			self.sendLine("Please input the right number you want to vote, please do not input nonnumeric character.")
 			return
-		if message<=10 and not message == 0:
+		self.food = findfood(`message`)
+		if message<=18 and not message == 0:
 			write = "%s,%s,%s\n" % (self.realname,self.number,message)
 			f.write(write) 
 			f.close()
 			self.vote = message
-			message = "%s<%s>vote:%s" % (self.realname,self.number,self.vote)
+			message = "%s<%s>vote:%s" % (self.realname,self.number,self.food)
 			print message
 			self.state = "voted"
-			self.sendLine("done , you are voted  Enter to see the result .")
+			self.sendLine("Thank you, your vote has been recorded. Hit 'Enter' to see the result .")
 		else:
-			self.sendLine("please Enter the number from 1 to 10")
+			self.sendLine("There are only 10 options, please Enter the number from 1 to 10.")
 			return
 	def handle_print(self,message):
 		self.sendLine(goodbye)
@@ -162,6 +179,7 @@ def processLine(wcDict) :
 	vote_result.close
 
 def printlist(wcDict,self) :
+	food_name = None
 	global totalcount
 	temp_total = totalcount
 	totalcount = 0
@@ -169,13 +187,14 @@ def printlist(wcDict,self) :
 	for key,val in wcDict.items():
 		valKeyList.append((val,key))
 	valKeyList.sort(reverse = True)
-	send_temp = '%-5s| %5s| %5s' % ('Item','Count','Probability')
+	send_temp = '  %-6s    |%5s|%5s' % ('name','count','  %  ')
 	self.sendLine (send_temp)
 	send_temp = '_'*25
 	self.sendLine (send_temp)
 	for val,key in valKeyList:
+		food_name = findfood(key)
 		Probability = val*100/float(temp_total)
-		send_temp = ' %-5s %3d %9.2f%%' % (key,val,Probability)
+		send_temp = '  %-12s|  %-4d|%5.2f%%|' % (food_name,val,Probability)
 		self.sendLine (send_temp)
 
 def print_result(self):
@@ -183,6 +202,34 @@ def print_result(self):
 	wcDict = {}
 	processLine(wcDict)
 	printlist(wcDict,self)
+
+def print_food(self):
+	width = 4
+	flag = False
+	format = '<%s> %-12s(%s)'
+	food_list = open("food_list",'r')
+	for line in food_list :
+		food_number = line.split(',')[0]
+		food_name     = line.split(',')[1]
+		food_price      = line.split(',')[2]
+		if flag :
+			send_temp = send_temp + format % (food_number,food_name,food_price)
+			self.sendLine(send_temp)
+		else:
+			send_temp = format % (food_number,food_name,food_price) + '  |  '
+		flag = not flag
+	food_list.close()
+
+def findfood(n):
+	temp = None
+	food_list = open("food_list",'rU')
+	for line in food_list :
+		food_number = line.split(',')[3]
+		food_name     = line.split(',')[1]
+		if food_number == n :
+			temp =  food_name
+	return temp		
+	food_list.close()
 
 reactor.listenTCP(PORT, voteFactory())
 reactor.run()
